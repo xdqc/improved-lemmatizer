1b337b627f262122e0b0401f042fbd3c41799257
Samuel <qichen.d@outlook.com>
2019-03-03 08:21:04 +1300

    Lemmatizer improvement
    Catch edge cases of common names and places
    Deal with -ves
    Add matching for -yss
    Deal with 'focus'

--- Lemmatizer.py
+++ Lemmatizer.py
@@ -5,33 +5,40 @@ def lemmatize(w: str):
   """
   Improved lemmatizer based on nltk.stem.WordNetLemmatizer
   """
-  if w in {'as', 'us', 'ms', 'mrs', 'vs', 'cos', 'wis', 'mis',
+  if w in {'as', 'us', 'ms', 'mrs', 
+           'vs', 'cos', 'wis', 'mis', 'gus',
            'des', 'les', 'las', 'pas', 'der', 'es', 'est',
-           'during', 'feed', 'mater',
-           'ground', 'clad', 'unclad',
+           'during', 'feed', 'mater', 
+           'ground', 'clad', 'unclad', 'infrared',
+           'viking', 'thames', 'kansas', 'alps',
+           'james', 'ellis', 'elias',
            'ss', 'os', 'hs', 'ps', 'cs', 'rs', 'bs', 'ds', 'fs', 'gs',
            'js', 'ks', 'ls', 'ns', 'qs', 'rs', 'ts', 'xs', 'ys', 'zs',
            'lest', 'lester', 'kester', 'vester', 'mester',
            'kest', 'cest', 'cester', 'dester'}:
     return w
 
-  if w in {'bees', 'axes'}:
+  if w in {'bees', 'axes', 'vikings'}:
     return w[:-1]
 
-  """ match open syllables 'consonate + vowel + consonate + e' """
+  if w in {'halves', 'calves', 'thieves'}:
+    return f'{w[:-3]}f'
+
+  """ match open syllables 'consonant + vowel + consonant + e' """
   match_op = re.match(
-      r'^([pbtdfvlmnghkczrwsx]{1,3})([aiou])([pbtdfvlmngkhczr])(es|ed|ing|ings)$', w)
+      r'^([pbtdfvlmnghkczrwsx]{1,3})([aiou])([pbtdfvlmnghkczr])(es|ed|ing|ings)$', w)
   if match_op:
     return f'{match_op[1]}{match_op[2]}{match_op[3]}e'
 
   """ match ending 'ss' """
-  match_ss = re.match(r'^([a-z]*)([aeiou])(ss)(es|ed|er|ing)?s?$', w)
+  match_ss = re.match(r'^([a-z]*)([aeiouy])(ss)(es|ed|er|ing)?s?$', w)
   if match_ss:
-    return f'{match_ss[1]}{match_ss[2]}{match_ss[3]}'
+    stub = f'{match_ss[1]}{match_ss[2]}{match_ss[3]}'
+    return re.sub(r'focuss$', 'focus', stub, 1)
 
   wnl = WordNetLemmatizer()
-  stub = wnl.lemmatize(w, pos='r')  # ADV
-  stub = wnl.lemmatize(stub, pos='a')  # ADV
+  stub = wnl.lemmatize(w,    pos='r')  # ADV
+  stub = wnl.lemmatize(stub, pos='a')  # ADJ
   stub = wnl.lemmatize(stub, pos='v')  # VERB
   stub = wnl.lemmatize(stub, pos='n')  # NOUN
   return stub
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Lemmatizer
+# Improved lemmatizer on top WordNetLemmatizer 
 
-## Improved lemmatizer based on nltk.stem.WordNetLemmatizer working on any part of speech
+`nltk.stem.WordNetLemmatizer` is (one of?) the best lemmatizer, and it can be better.
 
-Tested with top 100,000 most frequent words in the cocktail corpus of COCA, Google N-gram, Open Subtitle, Wikipedia.
+Tested with top 100,000 most frequent words in the [cocktail corpus](https://github.com/xdqc/english-corpus-words-frequency) of COCA (45%), Google Ngram (40%), Wikipedia (8%), Open Subtitle (4%) and Twitter (3%).
 
 ## Usage
 
@@ -17,27 +17,39 @@ Install NLTK https://www.nltk.org/install.html
 
  >>> lemmatize('as')
  as
- # WordNetLemmatizer returns 'u'
+ # WordNetLemmatizer returns 'a'
+
+ >>> lemmatize('during')
+ during
+ # WordNetLemmatizer returns 'dur'
 
  >>> lemmatize('rating')
  rate
  # WordNetLemmatizer returns 'rat'
 
- >>> lemmatize('wages')
- wage
- # WordNetLemmatizer returns 'wag'
+ >>> lemmatize('wares')
+ ware
+ # WordNetLemmatizer returns 'war'
 
- >>> lemmatize('passed')
- pass
- # WordNetLemmatizer returns 'pas'
+ >>> lemmatize('obsessing')
+ obsess
+ # WordNetLemmatizer returns 'ob'
 
  >>> lemmatize('assesses')
  assess
  # WordNetLemmatizer returns 'ass'
 
+ >>> lemmatize('abysses')
+ abyss
+ # WordNetLemmatizer returns 'ab'
+
  >>> lemmatize('feed')
  feed
- # WordNetLemmatizer returns 'fe'
+ # WordNetLemmatizer returns 'fee'
+
+ >>> lemmatize('ground')
+ ground
+ # WordNetLemmatizer returns 'grind'
 
  >>> lemmatize('bees')
  bee
